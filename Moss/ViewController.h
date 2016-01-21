561cef07439f8b90e3e80214f4d1540a274f4ec0
summer <summer.okay@gmail.com>
2016-01-21 17:38:41 +0800

    Clean Workspace

---
+++ Moss/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Moss
+//
+//  Created by Summer. on 16/1/21.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Moss/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Moss
+//
+//  Created by Summer. on 16/1/21.
+//  Copyright © 2016年 Summer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
